d7060f8f820af168b2b5e8fd2d4994b9b541d352
Chaithanya <Chaithanya>
2021-01-11 12:05:03 +0530

    Added log4j.properties

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,2 +1,17 @@
-public class Login {
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+public class Login extends TestBase {
+
+
+    private static final Logger LOGGER = Logger.getLogger(Login.class);
+
+    @Test
+    public void sampleTest(){
+        setUp();
+        System.out.println("Test");
+    }
+
+
+
 }
--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,4 +1,67 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TestBase {
 
+    private static Logger LOGGER = Logger.getLogger(TestBase.class);
 
+    public WebDriver webDriver;
+    public WebDriverWait webDriverWait;
+    private static Properties properties;
+
+
+    public WebDriver setUp() {
+        LOGGER.info("Current Method :: SetUp");
+        properties = loadProperties("config.properties", properties);
+        String browser = properties.getProperty("browser");
+
+        if (null == webDriver) {
+            switch (browser) {
+
+                case "Chrome":
+                    WebDriverManager.chromedriver().setup();
+                    webDriver = new ChromeDriver();
+                    break;
+
+                case "Firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    webDriver = new FirefoxDriver();
+                    break;
+
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    webDriver = new EdgeDriver();
+                    break;
+
+                case "ie":
+                    WebDriverManager.iedriver().setup();
+                    webDriver = new InternetExplorerDriver();
+                    break;
+
+            }
+        }
+
+        return webDriver;
+    }
+
+    public Properties loadProperties(String fileName , Properties props) {
+        try {
+            props = new Properties();
+            InputStream inputStream = TestBase.class.getResourceAsStream(fileName);
+            props.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
 }
